4b5f89bc6b33e06aac4e99487511bc032aef1d48
Krisliltech <mgbekwutechristian91@gmail.com>
2021-11-21 14:40:10 +0100

    Registration page implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import Home from './pages/Home';
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
+import Register from './pages/Register';
 
 function App() {
   return (
     <>
     {/* <Home /> */}
     {/* <ProductList /> */}
-    <Product />
+    {/* <Product /> */}
+    <Register />
     </>
   );
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,75 @@
-import React from 'react'
+import styled from "styled-components"
+
+const Container = styled.div`
+    width:100vw;
+    height: 100vh;
+    background-color:pink;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+// const Another  = styled.div`
+//     flex:1;
+//     width:100%
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+// `
+const Wrapper = styled.div`
+    // flex:1;  
+    width:40%;
+    padding: 20px;
+    background-color: whitesmoke;
+`
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0 0;
+    padding: 7px;
+
+`
+const Agreement = styled.span`
+    font-size: 12px;
+    margin: 20px 0;
+`
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+`
 
 const Register = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            {/* <Another>sdcfygvhjknlkgkfhgjh</Another> */}
+            <Wrapper>
+                <Title>CREATE AN ACCOUNT</Title>
+                <Form>
+                    <Input placeholder="first name"/>
+                    <Input placeholder="last name"/>
+                    <Input placeholder="username"/>
+                    <Input placeholder="email"/>
+                    <Input placeholder="password"/>
+                    <Input placeholder="confirm password"/>
+                    <Agreement>
+                        By creating an account, I consent to the processing of my personal
+                        data in accordance with the <b>PRIVACY POLICY</b>
+                    </Agreement>
+                    <Button>CREATE</Button>
+                </Form>
+            </Wrapper>
+        </Container>
     )
 }
 
